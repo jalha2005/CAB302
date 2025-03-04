c708f98c4b04b198c92f147f444e6651c4a96ef0
Wiriya Vang <wiriyavang@gmail.com>
2025-03-04 11:41:28 +1000

    version 4

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     private static ArrayList<User> users = new ArrayList<>();
 
     // Mock authentication service that always returns the first user when log in, and does nothing when sign up
-    private static IAuthenticationService authService = new IAuthenticationService() {
+    private static AuthenticationService authService = new AuthenticationService() {
         @Override
         public User signUp(String username, String password) {
             return null;
@@ -98,5 +98,4 @@ public class Main {
     }
 }
 
-// hello
-// dmmy
+
